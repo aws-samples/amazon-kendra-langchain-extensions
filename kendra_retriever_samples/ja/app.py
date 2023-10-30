994799b496c5137d6f5bf9e66aa6e39d3572aa5d
Naoki Miyaguchi <mynaoki@amazon.co.jp>
2023-10-30 10:33:48 +0900

    update: exception message

--- kendra_retriever_samples/ja/app.py
+++ kendra_retriever_samples/ja/app.py
@@ -55,9 +55,8 @@ if "llm_chain" not in st.session_state:
         else:
             raise Exception("Unsupported LLM: ", sys.argv[1])
     else:
-        raise Exception(
-            "Usage: streamlit run app.py <anthropic|flanxl|flanxxl|openai|bedrock_titan|bedrock_claude|bedrock|claudev2>"
-        )
+        raise Exception("Usage: streamlit run app.py <openai|falcon40b|bedrock_claude|bedrock|claudev2>")
+
 
 if "chat_history" not in st.session_state:
     st.session_state["chat_history"] = []
